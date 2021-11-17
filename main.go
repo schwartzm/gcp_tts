e343030eb59915f31d405b7695f927133d1bdf50
Michael Schwartz <michael.schwartz01@gmail.com>
2021-11-17 07:49:35 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	texttospeech "cloud.google.com/go/texttospeech/apiv1"
+	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
+)
+
+/*
+Rememeber to set the gcp auth key env path var before running.
+$env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\mike\arduino\gcp_keys\arduino-stuff-for-gcp-tts-4fd11c6824d0.json"
+
+Check env vars:
+get-childitem env:
+
+Go get all dependencies listed in all imports.
+go get -d ./...
+
+*/
+
+func main() {
+	// Instantiates a client.
+	ctx := context.Background()
+
+	client, err := texttospeech.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	// Perform the text-to-speech request on the text input with the selected
+	// voice parameters and audio file type.
+	req := texttospeechpb.SynthesizeSpeechRequest{
+		// Set the text input to be synthesized.
+		Input: &texttospeechpb.SynthesisInput{
+			InputSource: &texttospeechpb.SynthesisInput_Ssml{Ssml: "<speak><p>Question: What is the best Christmas present in the world?<break time=\"2.5s\"/>Answer: A broken drum, you just can’t beat it!</p></speak>"},
+		},
+		// Build the voice request, select the language code ("en-US") and the SSML
+		// voice gender ("neutral").
+		Voice: &texttospeechpb.VoiceSelectionParams{
+			LanguageCode: "en-US",
+			//SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
+			Name: "en-US-Wavenet-G",
+		},
+		// Select the type of audio file you want returned.
+		AudioConfig: &texttospeechpb.AudioConfig{
+			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+		},
+	}
+
+	resp, err := client.SynthesizeSpeech(ctx, &req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// The resp's AudioContent is binary.
+	filename := "en-US-Wavenet-G_sample.mp3"
+	err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Audio content written to file: %v\n", filename)
+}
